1ca3d68e07ec9126625ec544e63e528d568e074a
ilnur.nasibullin <ilnur.nasibullin@x5.ru>
2025-05-13 19:23:27 +0300

    product-service-init

---
+++ product-service/src/test/java/com/softi/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softi.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
